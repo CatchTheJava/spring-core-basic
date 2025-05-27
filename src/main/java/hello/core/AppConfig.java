fbdf6415bc152238e18b8d196aecfae0e967ebc8
aixion1506 <aixion1506@gmail.com>
2025-05-28 00:13:38 +0900

    Feat: AppConfig 클래스 생성하여 서비스 객체 구성 책임 분리

---
+++ src/main/java/hello/core/AppConfig.java
@@ -0,0 +1,19 @@
+package hello.core;
+
+import hello.core.discount.*;
+import hello.core.member.*;
+import hello.core.order.*;
+
+public class AppConfig {
+
+    public MemberService memberService() {
+        return new MemberServiceImpl(new MemoryMemberRepository());
+    }
+
+    public OrderService orderService() {
+        return new OrderServiceImpl(new MemoryMemberRepository(), new FixDiscountPolicy());
+    }
+
+
+
+}
