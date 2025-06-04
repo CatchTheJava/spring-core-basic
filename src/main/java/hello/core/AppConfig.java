7fcbd0e4bde69e107d715e1185b55d266d5e6ab1
aixion1506 <aixion1506@gmail.com>
2025-06-04 17:39:02 +0900

    Refactor: AppConfig 객체 생성 로직 메서드 분리

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -7,13 +7,20 @@ import hello.core.order.*;
 public class AppConfig {
 
     public MemberService memberService() {
-        return new MemberServiceImpl(new MemoryMemberRepository());
+        return new MemberServiceImpl(memberRepository());
+    }
+
+    private MemoryMemberRepository memberRepository() {
+        return new MemoryMemberRepository();
     }
 
     public OrderService orderService() {
-        return new OrderServiceImpl(new MemoryMemberRepository(), new FixDiscountPolicy());
+        return new OrderServiceImpl(memberRepository(), discountPolicy());
     }
 
+    public FixDiscountPolicy discountPolicy() {
+        return new FixDiscountPolicy();
+    }
 
 
 }
