85fbfb219636639d24ce193c0eaf136cb4825d3a
aixion1506 <aixion1506@gmail.com>
2025-06-20 17:38:58 +0900

    Refactor:  순수 Java 기반 객체 조립을 Spring DI 방식으로 전환

--- src/main/java/hello/core/OrderApp.java
+++ src/main/java/hello/core/OrderApp.java
@@ -2,13 +2,21 @@ package hello.core;
 
 import hello.core.member.*;
 import hello.core.order.*;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class OrderApp {
 
     public static void main(String[] args) {
-        AppConfig appConfig = new AppConfig();
-        MemberService memberService = appConfig.memberService();
-        OrderService orderService = appConfig.orderService();
+        // 순수 Java
+//        AppConfig appConfig = new AppConfig();
+//        MemberService memberService = appConfig.memberService();
+//        OrderService orderService = appConfig.orderService();
+
+        // Spring 코드
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+        MemberService memberService = applicationContext.getBean("memberService", MemberService.class);
+        OrderService orderService = applicationContext.getBean("orderService", OrderService.class);
 
         Long memberId = 1L;
         Member member = new Member(memberId, "memberA", Grade.VIP);
